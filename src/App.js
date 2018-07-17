3ad31c63cd3b58fccf81a85dbfcae18140bbfd4b
Tyler Earls <tyler.a.earls@gmail.com>
2018-07-17 16:14:51 -0500

    added get movie ajax request, wrote a basic custom error handling function

--- src/App.js
+++ src/App.js
@@ -10,23 +10,58 @@ class App extends Component {
     super();
     this.state = {
       apiKey: '974778d7', // personal API key activated from omdbapi.com
-      selectedMovie: ''
+      domain: 'localhost:3000', // will eventually be a heroku url
+      selectedMovie: {}
     }
   }
 
-  getMovie = (movieTitle) => {
+  getMovie = async (movieTitle) => {
+    // convert spaces in movieTitle to + signs
+    const search = await fetch('http://www.omdbapi.com/?t=' + movieTitle + '&apiKey=' + this.state.apiKey);
+    const searchJson = await search.json();
+    if (searchJson.Response === "True") {
+      this.setState({
+        selectedMovie: {
+          title: searchJson.Title,
+          rated: searchJson.Rated,
+          year: searchJson.Year,
+          runtime: searchJson.Runtime,
+          genre: searchJson.Genre,
+          plot: searchJson.Plot,
+          poster: searchJson.Poster,
+          website: searchJson.Website,
+          awards: searchJson.Awards,
+          actors: searchJson.Actors,
+          production: searchJson.Production,
+          ratings: {
+            "IMDB": {
+              score: "IMDB: " + searchJson.Ratings[0].Value
+            },
+            "Rotten Tomatoes": {
+              score: "Rotten Tomatoes: " + searchJson.Ratings[1].Value
+            },
+            "Metacritic": {
+              score: "Metacritic: " + searchJson.Ratings[2].Value
+            }
+          }
+        }
+      });
+      console.log(this.state.selectedMovie);
+    } else {
+      this.errorMessage(movieTitle);
+    }
+  }
 
+  errorMessage = (movieTitle) => {
+    console.log(movieTitle, "is not found.");
   }
 
   render() {
+    // this.getMovie('Shrek');
+    // console.log(this.getMovie('Finding Nemo'));
     return (
       <div className={styles.app}>
         <MovieContainer />
-        <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <h2>Sass is working</h2>
-          <span role="img" aria-label="emoji">😎</span>
-        </header>
       </div>
     );
   }
