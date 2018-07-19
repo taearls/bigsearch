20fe239817ef65d49e3f33b4bf14f4984e1b0ffc
Tyler Earls <tyler.a.earls@gmail.com>
2018-07-19 02:19:38 -0500

    updated styles and jsx of Footer container

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,8 +6,8 @@ export default class Footer extends Component {
   render() {
     return (
       <div className={styles.footer}>
-        <h3> Search by typing the movie title in the URL. </h3>
-        <h3> Example: {this.props.domain}/big </h3>
+        <h3> Search by appending the movie title in the URL: </h3>
+        <h4> Example: {this.props.domain}/big </h4>
       </div>
     )
   }
